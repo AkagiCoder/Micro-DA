0d409ab0bbfa57c9908a270bdcdc0cc9b5c91920
AkagiCoder <35548639+AkagiCoder@users.noreply.github.com>
2018-03-28 20:49:04 -0700

    Added Final and Updated DA3

---
+++ Final-Design/Non-Linear_ODE[Lorenz]/Non-Linear_ODE[Lorenz]/main.c
@@ -0,0 +1,30 @@
+/*
+ * Non-Linear_ODE[Lorenz].c
+ *
+ * Created: 3/27/2018 11:53:41 PM
+ * Author : Bryan
+ */ 
+
+#include <avr/io.h>
+#include <stdint.h>
+
+#define A 10
+#define B 8/3
+#define C 28
+
+volatile int8_t x;
+volatile int8_t y;
+volatile int8_t z;
+
+volatile int8_t dx;
+volatile int8_t dy;
+volatile int8_t dz;
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
